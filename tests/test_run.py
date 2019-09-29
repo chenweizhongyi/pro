e7abacdde6f3a39a3bd2bb3ca09f329861107ebb
Antony <tester@test.com>
2019-09-29 11:53:48 +0800

    测试

--- tests/test_run.py
+++ tests/test_run.py
@@ -8,7 +8,6 @@ def load_testcase(case_path):
 
 def run_single_test_case(testcase):
     req_kwargs = testcase['request']
-
     try:
         url = req_kwargs.pop('url')
         method = req_kwargs.pop('method')
@@ -22,18 +21,35 @@ def run_single_test_case(testcase):
     return sucess,diff
 
 def diff_response(resp_obj, expected_resp_json):
-    diff_content = {}
+    # diff_content = {}
     resp_info = parse_response_object(resp_obj)
     print(type(resp_info),':resp_info:',resp_info)
-    if resp_info['status_code'] not in expected_resp_json['status_code']:
-        diff_content['status_code'] = resp_info['status_code']
-    if resp_info['headers'] not in expected_resp_json['headers']:
-        diff_content['headers'] = resp_info['headers']
-    if resp_info['body'] not in expected_resp_json['body']:
-        diff_content['body'] = resp_info['body']
+    diff_content = diff_json(resp_info,expected_resp_json)
     return diff_content
 
+def diff_json(current_json, expected_json):
+    """
+    比较实际结果与预期结果
+    :param current_json:
+    :param expected_json:
+    :return:
+    """
+    json_diff = {}
+    for key,expected_value in expected_json.items():
+        value = current_json[key]
+        if str(value) != str(expected_value):
+            json_diff[key]={
+                'value':value,
+                'expected_value':expected_value
+            }
+    return json_diff
+
 def parse_response_object(resp_obj):
+    """
+    将实际结果解析字典格式返回
+    :param resp_obj:
+    :return:
+    """
     try:
         resp_resp = resp_obj.json()
     except Exception:
